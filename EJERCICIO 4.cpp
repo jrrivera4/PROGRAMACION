de1eb5799d56ade4f44cd0445f53c695989617c2
jrrivera4 <139582369+jrrivera4@users.noreply.github.com>
2023-07-14 13:33:01 -0500

    Add files via upload

---
+++ EJERCICIO 4.cpp
@@ -0,0 +1,21 @@
+//Ejecicio_4 JR//
+//Hacer un programa que muestre una tabla de multiplicar hasta el 20 de un número//
+//cualquiera por pantalla, el número se pedirá en el programa principal. Usar procedimiento//
+#include <iostream>
+using namespace std;
+
+void tablaMultiplicar(int num) {
+    for (int i = 1; i <= 20; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+}
+
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    tablaMultiplicar(num);
+    return 0;
+}
+
+
---
+++ EJERCICIO 6.cpp
@@ -0,0 +1,28 @@
+//Ejercicio 6 JR//
+//Utilizar la función fopen(), para determinar si existe un archivo de texto (.txt)//
+//o no.//
+//fopen(nombre de archivo , modo);//
+//nombre de archivo = cadena ... contiene el identificar externo del archivo//
+//modo = cadena ... contiene el modo en que va a ser tratado el archivo//
+#include <stdio.h>
+
+int main()
+{
+    FILE *archivo;
+    char nombreArchivo[] = "archivo.txt";
+
+    archivo = fopen(nombreArchivo, "r");
+
+    if (archivo == NULL)
+    {
+        printf("El archivo no existe.\n");
+    }
+    else
+    {
+        printf("El archivo existe.\n");
+        fclose(archivo);
+    }
+
+    return 0;
+}
+
